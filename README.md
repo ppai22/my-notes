d41d2a6cc89b78823c4f448f7cafbe688d537f66
ppai22 <thottam.pradeepkrishna@gmail.com>
2019-12-19 22:41:25 +0530

    Update

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 |LINKS|
 |------|
-|[1 - List Comprehensions](.\Notes\Lists.md)|
-|[2 - Flatten a list of lists](.\Notes\Lists.md)|
+|[1 - List Comprehensions](https://github.com/ppai22/my-notes/blob/master/Notes/Lists.md#1---list-comprehensions)|
+|[2 - Flatten a list of lists](https://github.com/ppai22/my-notes/blob/master/Notes/Lists.md#2---flatten-a-list-of-lists)|
--- generate_links.py
+++ generate_links.py
@@ -2,6 +2,7 @@ import re
 import os
 
 
+repo = r'https://github.com/ppai22/my-notes/blob/master/Notes/'
 notes = list()
 for root, dirs, files in os.walk(r'.\Notes'):
     for file in files:
@@ -10,7 +11,9 @@ for root, dirs, files in os.walk(r'.\Notes'):
                 md_lines = md.readlines()
                 for line in md_lines:
                     if re.match(r'##\s', line):
-                        notes.append((line.split('## ')[-1].strip('\n'), root + '\\' + file))
+                        note_title = line.split('## ')[-1].strip('\n')
+                        note_link = re.sub(' ', '-', note_title.lower())
+                        notes.append((note_title, repo + file + '#' + note_link))
 
 with open(r'.\README.md', 'w+') as readme:
     readme.write('# My personal notes on Python\n\n')
