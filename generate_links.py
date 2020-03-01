a7bdd582eb21c7b320b5a4966b0db9eb13f3f2f7
ppai22 <thottam.pradeepkrishna@gmail.com>
2020-03-01 16:47:45 +0530

    Modifies logic

--- generate_links.py
+++ generate_links.py
@@ -3,7 +3,7 @@ import os
 
 
 repo = r'https://github.com/ppai22/my-notes/blob/master/Notes/'
-categories = dict()
+articles = dict()
 for root, dirs, files in os.walk(r'.\Notes'):
     for file in files:
         if file.split('.')[-1] == 'md':
@@ -11,21 +11,27 @@ for root, dirs, files in os.walk(r'.\Notes'):
             notes = list()
             with open('.\\Notes\\' + file, 'r+') as md:
                 md_lines = md.readlines()
-                for line in md_lines:
-                    if re.match(r'##\s', line):
-                        note_title = line.split('## ')[-1].strip('\n')
+                for line in range(len(md_lines)):
+                    if re.match(r'##\s', md_lines[line]):
+                        note_title = md_lines[line].split('## ')[-1].strip('\n')
                         note_link = re.sub(' ', '-', note_title.lower())
                         note_link = re.sub('[.()/,]', '', note_link.lower())
-                        notes.append((note_title, repo + file + '#' + note_link))
-            categories[name] = notes
+                        if re.match(r'Tags:', md_lines[line+1]):
+                            tags = md_lines[line+1].split('Tags:')[-1].strip().strip('\n').split('<br>')[0]
+                        else:
+                            tags = []
+                        notes.append((note_title, repo + file + '#' + note_link, tags))
+            articles[name] = notes
+print(articles)
+
 
 with open(r'.\README.md', 'w+') as readme:
     readme.write('# My personal notes on Python\n\n')
     readme.write('|LINKS|\n')
     readme.write('|------|\n')
-    for category in categories.keys():
-        readme.write('|' + '**' + category + '**' + '|' + '\n')
+    for name in articles.keys():
+        readme.write('|' + '**' + name + '**' + '|' + '\n')
         readme.write('|' + '<ul>')
-        for note in categories[category]:
+        for note in articles[name]:
             readme.write('<li>' + '[' + note[0] + ']' + '(' + note[1] + ')' + '</li>')
         readme.write('</ul>' + '|' + '\n')
